c904d4fe366dfe06f4b4625bdbb7c5e182840b1d
IanGametion <iansinarfa@gmail.com>
2025-05-21 15:45:37 +0700

    fix

--- Addmatrix.py
+++ Addmatrix.py
@@ -4,18 +4,36 @@ def create_Mtx2D(row, col):
     i = 0
     j = 0
     mtx = []
-    while i < row:
+    for i in range(row):
         rw = []
-        while j < col:
-            element = int(input("Enter [" + str(i+1) + "][" + str(j+1) + "]: "))
+        for j in range(col):
+            m = i + 1
+            n = j + 1
+            element = int(input("Enter [" + str(m) + "][" + str(n) + "]: "))
             rw.append(element)
-            j = j+1
         mtx.append(rw)
-        i = i + 1
     return mtx
-            
+
+def print_Mtx2D(mtx):
+    for row in mtx:
+        print(row)
+
+def add_Mtx2D(mtx1, mtx2):
+    rw, cl = len(mtx1), len(mtx1[0])
+    mtx = [[0]* cl] * rw
+    mtx = [[mtx1[i][j] + mtx2[i][j] for j in range(cl)] for i in range(rw)]
+    return mtx
 
 row = int(input("Enter number of rows: "))
 col = int(input("Enter number of columns: "))
 print("Enter first matrix: ")
 mtx1 = create_Mtx2D(row, col)
+print("Enter second matrix: ")
+mtx2 = create_Mtx2D(row, col)
+print("First matrix: ")
+print_Mtx2D(mtx1)
+print("Second matrix: ")
+print_Mtx2D(mtx2)
+mtx = add_Mtx2D(mtx1, mtx2)
+print("Results:")
+print_Mtx2D(mtx)
