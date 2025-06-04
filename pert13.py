d840d467ce724fcc345551afeb00d7d77d45f4fc
IanGametion <iansinarfa@gmail.com>
2025-06-04 14:12:28 +0700

    Syntax

--- pert13.py
+++ pert13.py
@@ -13,3 +13,27 @@ class Stack:
     
     def is_empty(self):
         return len(self.items) == 0
+    
+class SyntaxChecker:
+    def check_balanced(filename):
+        try:
+            with open(filename, 'r') as file:
+                stack = Stack()
+                while True:
+                    c = file.read(1)
+                    if not c:
+                        break
+                    if c in "([{":
+                        stack.push(c)
+                    elif c in ")]}":
+                        if stack.is_empty():
+                            return 0
+                        opening_ch = stack.pop()
+                        if (c == ')' and opening_ch != '(') or \
+                           (c == ']' and opening_ch != '[') or \
+                           (c == '}' and opening_ch != '{'):
+                            return 0
+                return 1 if stack.is_empty() else 0
+        except IOError as e:
+            print(e)
+            return 0
